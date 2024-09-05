fdce4e0682a25ed5842c13cb7a9ecc7651b30a1c
harishsundhar <harishsundhar@outlook.com>
2024-09-05 09:04:46 +0530

    update status

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,21 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import { tasksController } from '../controller/tasksController';
-
+import { Config } from "../config/auth";
 
 export class Routes {
     private tasks: tasksController = new tasksController();
-    
+    private config = new Config();
     public routes(app): void {
           app.route('/health/entity-service').get((req: Request, res: Response) => {
             res.status(200).send({
                 status: 'up'
             })
         })
-        app.route('/tasks/:id').delete(this.tasks.Delete);
-        app.route('/tasks').put(this.tasks.Update);
-        app.route('/tasks/:id').get(this.tasks.GetEntityById);
-        app.route('/tasks').get(this.tasks.GetAllValues);
-        app.route('/tasks').post(this.tasks.Create);
+        app.route('/tasks/:id').delete(this.config.authenticateToken, this.tasks.Delete);
+        app.route('/tasks').put(this.config.authenticateToken, this.tasks.Update);
+        app.route('/tasks/:id').get(this.config.authenticateToken, this.tasks.GetEntityById);
+        app.route('/tasks').get(this.config.authenticateToken, this.tasks.GetAllValues);
+        app.route('/tasks').post(this.config.authenticateToken, this.tasks.Create);
         app.route('/login').post(this.tasks.login);
         app.route('/signup').post(this.tasks.signup);
      }
